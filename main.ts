6f88717eef838c7ab8727338d05c7288948eafb9
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-09 18:42:48 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+let state = 0
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+        basic.pause(10)
+        if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+            state = 1
+        }
+    }
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.pause(10)
+        if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+            state = 0
+        }
+    }
+})
+basic.forever(function () {
+    if (state) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
 })
